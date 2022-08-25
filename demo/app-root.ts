f50b80fd52486dce2ac79a93d49809fdb04c064a
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2022-08-25 11:08:48 -0700

    checkpoint: file crawl & beef up track details + prep demo for data viz

--- demo/app-root.ts
+++ demo/app-root.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-return-assign */
 /* eslint-disable no-restricted-globals */
 import { html, css, LitElement, TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
@@ -8,6 +9,9 @@ import '../src/players/youtube-player';
 
 @customElement('app-root')
 export class AppRoot extends LitElement {
+  @property({ type: String, reflect: true }) viewToShow: 'components' | 'data' =
+    'components';
+
   @property({ type: String }) selectedByDropdown: channelTypes =
     channelTypes.beta;
 
@@ -16,6 +20,15 @@ export class AppRoot extends LitElement {
   @property({ type: String }) selectedByRadio: channelTypes = channelTypes.beta;
 
   @property({ type: String }) selectedByRadioOnload: channelTypes | '' = '';
+
+  override updated(changed: Record<string, any>): void {
+    if (changed.has('viewToShow')) {
+      document
+        .querySelector('body')
+        ?.removeAttribute(changed.get('viewToShow'));
+      document.querySelector('body')?.setAttribute(this.viewToShow, '');
+    }
+  }
 
   get startAtWebamp() {
     const searchParams = new URLSearchParams(location.search.slice(1));
@@ -55,61 +68,90 @@ export class AppRoot extends LitElement {
   }
 
   render() {
+    return html`
+      <section id="app-root">
+        <h1>
+          Music Player Things:
+          <button @click=${() => (this.viewToShow = 'components')}>
+            Components
+          </button>
+          <button @click=${() => (this.viewToShow = 'data')}>Data</button>
+        </h1>
+        <hr />
+        <hr />
+        <br /><br />
+        ${this.viewToShow === 'components'
+          ? this.componentsView
+          : this.dataView}
+      </section>
+    `;
+  }
+
+  get dataView(): TemplateResult {
+    return html`
+      <section id="data">
+        <div></div>
+      </section>
+    `;
+  }
+
+  get componentsView(): TemplateResult {
     const url = `${location.origin}/demo`;
     return html`
-      <br />
-      <channel-selector
-        spotify
-        youtube
-        beta
-        samples
-        .selected=${this.startAtWebamp
-          ? channelTypes.webamp
-          : this.selectedByRadio}
-        @postInit=${(e: CustomEvent) => {
-          this.selectedByRadioOnload = e.detail.channel as channelTypes;
-        }}
-        @channelChange=${(e: CustomEvent) => {
-          this.selectedByRadio = e.detail.channel as channelTypes;
-        }}
-        .url=${url}
-      >
-      </channel-selector>
-
-      <section class="player">${this.playerByRadio}</section>
-      <section class="details">
-        <h2>Selected by radio</h2>
-        <h2>on first load: ${this.selectedByRadioOnload}</h2>
-        <h2>on change: ${this.selectedByRadio}</h2>
-      </section>
-
-      <channel-selector
-        spotify
-        youtube
-        beta
-        .displayStyle=${'dropdown'}
-        .url=${url}
-        @postInit=${(e: CustomEvent) => {
-          this.selectedByDropdownOnload = e.detail.channel as channelTypes;
-        }}
-        @channelChange=${(e: CustomEvent) => {
-          this.selectedByDropdown = e.detail.channel as channelTypes;
-        }}
-        .selected=${
-          // eslint-disable-next-line no-nested-ternary
-          this.startAtWebamp
+      <section id="components">
+        <channel-selector
+          spotify
+          youtube
+          beta
+          samples
+          .selected=${this.startAtWebamp
             ? channelTypes.webamp
-            : this.selectedByDropdown
-            ? this.selectedByDropdown
-            : channelTypes.beta
-        }
-      >
-      </channel-selector>
-      <section class="player">${this.playerByDropdown}</section>
-      <section class="details">
-        <h2>Selected by dropdown</h2>
-        <h2>on first load: ${this.selectedByDropdownOnload}</h2>
-        <h2>on change: ${this.selectedByDropdown}</h2>
+            : this.selectedByRadio}
+          @postInit=${(e: CustomEvent) => {
+            this.selectedByRadioOnload = e.detail.channel as channelTypes;
+          }}
+          @channelChange=${(e: CustomEvent) => {
+            this.selectedByRadio = e.detail.channel as channelTypes;
+          }}
+          .url=${url}
+        >
+        </channel-selector>
+
+        <section class="player">${this.playerByRadio}</section>
+        <section class="details">
+          <h2>Selected by radio</h2>
+          <h2>on first load: ${this.selectedByRadioOnload}</h2>
+          <h2>on change: ${this.selectedByRadio}</h2>
+        </section>
+
+        <channel-selector
+          spotify
+          youtube
+          beta
+          .displayStyle=${'dropdown'}
+          .url=${url}
+          @postInit=${(e: CustomEvent) => {
+            this.selectedByDropdownOnload = e.detail.channel as channelTypes;
+          }}
+          @channelChange=${(e: CustomEvent) => {
+            this.selectedByDropdown = e.detail.channel as channelTypes;
+          }}
+          .selected=${
+            // eslint-disable-next-line no-nested-ternary
+            this.startAtWebamp
+              ? channelTypes.webamp
+              : this.selectedByDropdown
+              ? this.selectedByDropdown
+              : channelTypes.beta
+          }
+        >
+        </channel-selector>
+        <section class="player">${this.playerByDropdown}</section>
+        <section class="details">
+          <h2>Selected by dropdown</h2>
+          <h2>on first load: ${this.selectedByDropdownOnload}</h2>
+          <h2>on change: ${this.selectedByDropdown}</h2>
+        </section>
       </section>
     `;
   }
@@ -118,7 +160,6 @@ export class AppRoot extends LitElement {
     :host {
       display: block;
       position: relative;
-      color: #fff;
     }
 
     .details {
