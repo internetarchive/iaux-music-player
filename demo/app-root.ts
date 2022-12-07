200a8e1a611e8b134be1fbb1ddb67fc93ef92806
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2022-12-06 17:24:59 -0800

    Data modeling album, tracks (#10)

--- demo/app-root.ts
+++ demo/app-root.ts
@@ -1,21 +1,117 @@
+/* eslint-disable no-return-assign */
 /* eslint-disable no-restricted-globals */
-import { html, css, LitElement, TemplateResult } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { html, css, LitElement, TemplateResult, nothing } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
+import { MetadataResponse } from '@internetarchive/search-service';
 import { channelTypes } from '../src/channel-selector/channels';
 import '../src/channel-selector/channel-selector';
 import '../src/players/spotify-player';
 import '../src/players/youtube-player';
+import { Album } from '../src/models/album';
+import { PlaylistTrack } from '../src/models/track';
 
+const albumList = [
+  {
+    id: 'cd_first-life_various-artists',
+    desc: 'CD - with no Liner notes, yes YT, yes SP, no Webamp',
+  },
+  {
+    id: 'lp_the-dark-side-of-the-moon_pink-floyd',
+    desc: 'LP - Pink Floyd Dark Side of the Moon',
+  },
+  {
+    id: 'cd_the-dark-side-of-the-moon_pink-floyd',
+    desc: 'CD - Pink Floyd Dark Side of the Moon',
+  },
+  {
+    id: 'capitol-15045-b-cigarettes-whiskey-and-wild-wild-women',
+    desc: '78 - w/o jp2 (only 1 item image)',
+  },
+  {
+    id: 'bestofdollyparto00part',
+    desc: 'LP - older',
+  },
+  {
+    id: 'lp_dancing-tonight_freddy-martin-and-his-orchestra',
+    desc: 'LP - current, ~ 2020',
+  },
+  {
+    id: 'cd_beethoven-complete-works-for-string-trio_the-adaskin-string-trio',
+    desc: 'what_cd',
+  },
+  {
+    id: 'wcd_message-in-a-box-th_the-police_flac_lossless_807968',
+    desc: 'Irregular Photo - (portrait)',
+  },
+  {
+    id: 'lak-JC_Burris-James_Booker',
+    desc: 'No photo + long track list',
+  },
+  {
+    id: 'wcd_various-artiststhe-best-of-country-music_flac_lossless_29887623',
+    desc: 'Complilation, various artists',
+  },
+  {
+    id: 'lp_emperor-concerto_ludwig-van-beethoven-arthur-rubinstein-bos',
+    desc: 'Track names, multiple but same as album artist (should be omitted)',
+  },
+  {
+    id: 'illegal-art',
+    desc: '3 column track list wide view pagination check',
+  },
+  {
+    id: 'wcd_borghild_die-warzau_mp3_320_1648819',
+    desc: 'Track time display, 60 seconds adds another minute. should display as 10:00',
+  },
+  {
+    id: 'cd_aaliyah_aaliyah-static-from-playa-timbaland',
+    desc: 'Has 3rd party "Full Album". Clicking on Full Album should highlight full album',
+  },
+];
 @customElement('app-root')
 export class AppRoot extends LitElement {
+  @property({ type: String, reflect: true }) viewToShow: 'components' | 'data' =
+    'data';
+
   @property({ type: String }) selectedByDropdown: channelTypes =
     channelTypes.beta;
+
+  @property({ type: String }) errorMsg: string = '';
 
   @property({ type: String }) selectedByDropdownOnload: channelTypes | '' = '';
 
   @property({ type: String }) selectedByRadio: channelTypes = channelTypes.beta;
 
   @property({ type: String }) selectedByRadioOnload: channelTypes | '' = '';
+
+  @property({ type: String }) albumId: string = '';
+
+  @property({ type: Object, attribute: false }) albumMd: Record<
+    string,
+    any
+  > | null = null;
+
+  @property({ type: Object, attribute: false }) albumPlaylist: Record<
+    string,
+    any
+  > | null = null;
+
+  @property({ type: Object, attribute: false }) album: Album | null = null;
+
+  @query('input#md-search') input!: HTMLInputElement;
+
+  override updated(changed: Record<string, any>): void {
+    if (changed.has('viewToShow')) {
+      document
+        .querySelector('body')
+        ?.removeAttribute(changed.get('viewToShow'));
+      document.querySelector('body')?.setAttribute(this.viewToShow, '');
+    }
+
+    if (changed.has('albumId') && this.albumId) {
+      this.albumInfo();
+    }
+  }
 
   get startAtWebamp() {
     const searchParams = new URLSearchParams(location.search.slice(1));
@@ -55,61 +151,262 @@ export class AppRoot extends LitElement {
   }
 
   render() {
+    return html`
+      <section id="app-root">
+        <h1>
+          Music Player Things:
+          <button @click=${() => (this.viewToShow = 'components')}>
+            Components
+          </button>
+          <button @click=${() => (this.viewToShow = 'data')}>Data</button>
+
+          ${this.viewToShow === 'data'
+            ? html`<div>
+                <form @submit=${(e: Event) => this.formInputSubmit(e)}>
+                  <label
+                    ><span>Item ID:</span
+                    ><input
+                      id="md-search"
+                      placeholder=""
+                      .value=${this.albumId}
+                  /></label>
+                </form>
+                <button
+                  @click=${() => {
+                    this.albumId = '';
+                    this.album = null;
+                    this.albumMd = null;
+                    this.albumPlaylist = null;
+                    this.errorMsg = '';
+                  }}
+                >
+                  CLEAR
+                </button>
+              </div>`
+            : nothing}
+        </h1>
+        <hr />
+        <hr />
+        <br /><br />
+        ${this.viewToShow === 'components'
+          ? this.componentsView
+          : this.dataView}
+      </section>
+    `;
+  }
+
+  async albumInfo(): Promise<void> {
+    this.errorMsg = '';
+    try {
+      const md = await fetch(
+        `https://archive.org/metadata/${this.albumId}`
+      ).then(res => res.json());
+      const playlist = await fetch(
+        `https://archive.org/services/playlist/${this.albumId}`
+      ).then(res => res.json());
+      this.albumPlaylist = playlist;
+      this.albumMd = new MetadataResponse(md);
+      this.album = new Album(
+        this.albumMd as MetadataResponse,
+        this.albumPlaylist as PlaylistTrack[]
+      );
+      (window as any).Album = this.album;
+    } catch (e: any) {
+      this.errorMsg = e.message;
+    }
+  }
+
+  formInputSubmit(e: Event): void {
+    e.preventDefault();
+    this.albumId = this.input.value;
+  }
+
+  get albumStats(): TemplateResult | typeof nothing {
+    if (!this.album) {
+      return nothing;
+    }
+
+    const spTrackNums = (
+      this.album.spotifyTracks.reduce((acc: (number | string)[], tr) => {
+        acc.push(tr?.track || 'n/a');
+        return acc;
+      }, []) as number[]
+    ).join(', ');
+    const ytTrackNums = (
+      this.album.youtubeTracks.reduce((acc: (number | string)[], tr) => {
+        acc.push(tr?.track || 'n/a');
+        return acc;
+      }, []) as number[]
+    ).join(', ');
+
+    return html`
+      <section id="album-stats">
+        <h2>Stats</h2>
+        <dl>
+          <dr
+            ><dt>
+              <img
+                class="album-image"
+                src=${this.album.albumImage}
+                alt="item img"
+              />
+            </dt>
+            <dd>Image: ${this.album.albumImage}</dd></dr
+          >
+          <dr
+            ><dt>Title</dt>
+            <dd>${this.album.title}</dd></dr
+          >
+          <dr
+            ><dt>Creator</dt>
+            <dd>${this.album.creator}</dd></dr
+          >
+          <dr
+            ><dt>Liner Notes?</dt>
+            <dd>${this.album.linerNotes.length}</dd></dr
+          >
+          <dr
+            ><dt>Images?</dt>
+            <dd>${this.album.images.length}</dd></dr
+          >
+          <dr
+            ><dt>/services/playlist Count</dt>
+            <dd>${this.album.rawPlaylistTracks.length}</dd></dr
+          >
+          <dr
+            ><dt>Tracks Count Found</dt>
+            <dd>${this.album.tracks.length}</dd></dr
+          >
+          <dr
+            ><dt>YT Track #</dt>
+            <dd>${this.album.youtubeTracks.length} => ${ytTrackNums}</dd></dr
+          >
+          <dr
+            ><dt>Has YT Album?</dt>
+            <dd>${this.album.youtubeId ? this.album.youtubeId : 'NO'}</dd></dr
+          >
+          <dr
+            ><dt>Spotify Track</dt>
+            <dd>${this.album.spotifyTracks.length} => ${spTrackNums}</dd></dr
+          >
+          <dr
+            ><dt>Has Spotify Album?</dt>
+            <dd>${this.album.spotifyId ? this.album.spotifyId : 'NO'}</dd></dr
+          >
+        </dl>
+      </section>
+    `;
+  }
+
+  get demoClicks(): TemplateResult {
+    return html`
+      <section id="demo-clicks">
+        ${albumList.map((alb: Record<string, any>) => {
+          const isSelected = this.albumId === alb.id;
+          return html`
+            <div class=${`demo ${isSelected ? 'selected' : ''}`}>
+              <button class="demo-go" @click=${() => (this.albumId = alb.id)}>
+                GO
+              </button>
+              <p><b>ID: ${alb.id}</b></p>
+              <p>${alb.desc}</p>
+            </div>
+          `;
+        })}
+      </section>
+    `;
+  }
+
+  get dataView(): TemplateResult {
+    return html`
+      <section id="data">
+        <div></div>
+        ${this.demoClicks}
+        ${this.errorMsg
+          ? html`<h2 id="error">ERROR: ${this.errorMsg}</h2>`
+          : nothing}
+
+        <h2>
+          Info for:
+          <a
+            _target="blank"
+            href=${`https:/archive.org/details/${this.albumId}`}
+            >${this.albumId}</a
+          >
+        </h2>
+
+        ${this.albumStats}
+        <div>
+          <iframe
+            src=${`https://archive.org/details/${this.albumId}`}
+            title="ia details page"
+          ></iframe>
+        </div>
+        <div>
+          <h3>Metadata</h3>
+          <pre>${JSON.stringify(this.albumPlaylist)}</pre>
+        </div>
+      </section>
+    `;
+  }
+
+  get componentsView(): TemplateResult {
     const url = `${location.origin}/demo`;
     return html`
-      <br />
-      <channel-selector
-        spotify
-        youtube
-        beta
-        samples
-        .selected=${this.startAtWebamp
-          ? channelTypes.webamp
-          : this.selectedByRadio}
-        @postInit=${(e: CustomEvent) => {
-          this.selectedByRadioOnload = e.detail.channel as channelTypes;
-        }}
-        @channelChange=${(e: CustomEvent) => {
-          this.selectedByRadio = e.detail.channel as channelTypes;
-        }}
-        .url=${url}
-      >
-      </channel-selector>
-
-      <section class="player">${this.playerByRadio}</section>
-      <section class="details">
-        <h2>Selected by radio</h2>
-        <h2>on first load: ${this.selectedByRadioOnload}</h2>
-        <h2>on change: ${this.selectedByRadio}</h2>
-      </section>
-
-      <channel-selector
-        spotify
-        youtube
-        beta
-        .displayStyle=${'dropdown'}
-        .url=${url}
-        @postInit=${(e: CustomEvent) => {
-          this.selectedByDropdownOnload = e.detail.channel as channelTypes;
-        }}
-        @channelChange=${(e: CustomEvent) => {
-          this.selectedByDropdown = e.detail.channel as channelTypes;
-        }}
-        .selected=${
-          // eslint-disable-next-line no-nested-ternary
-          this.startAtWebamp
+      <section id="components">
+        <channel-selector
+          spotify
+          youtube
+          beta
+          samples
+          .selected=${this.startAtWebamp
             ? channelTypes.webamp
-            : this.selectedByDropdown
-            ? this.selectedByDropdown
-            : channelTypes.beta
-        }
-      >
-      </channel-selector>
-      <section class="player">${this.playerByDropdown}</section>
-      <section class="details">
-        <h2>Selected by dropdown</h2>
-        <h2>on first load: ${this.selectedByDropdownOnload}</h2>
-        <h2>on change: ${this.selectedByDropdown}</h2>
+            : this.selectedByRadio}
+          @postInit=${(e: CustomEvent) => {
+            this.selectedByRadioOnload = e.detail.channel as channelTypes;
+          }}
+          @channelChange=${(e: CustomEvent) => {
+            this.selectedByRadio = e.detail.channel as channelTypes;
+          }}
+          .url=${url}
+        >
+        </channel-selector>
+
+        <section class="player">${this.playerByRadio}</section>
+        <section class="details">
+          <h2>Selected by radio</h2>
+          <h2>on first load: ${this.selectedByRadioOnload}</h2>
+          <h2>on change: ${this.selectedByRadio}</h2>
+        </section>
+
+        <channel-selector
+          spotify
+          youtube
+          beta
+          .displayStyle=${'dropdown'}
+          .url=${url}
+          @postInit=${(e: CustomEvent) => {
+            this.selectedByDropdownOnload = e.detail.channel as channelTypes;
+          }}
+          @channelChange=${(e: CustomEvent) => {
+            this.selectedByDropdown = e.detail.channel as channelTypes;
+          }}
+          .selected=${
+            // eslint-disable-next-line no-nested-ternary
+            this.startAtWebamp
+              ? channelTypes.webamp
+              : this.selectedByDropdown
+              ? this.selectedByDropdown
+              : channelTypes.beta
+          }
+        >
+        </channel-selector>
+        <section class="player">${this.playerByDropdown}</section>
+        <section class="details">
+          <h2>Selected by dropdown</h2>
+          <h2>on first load: ${this.selectedByDropdownOnload}</h2>
+          <h2>on change: ${this.selectedByDropdown}</h2>
+        </section>
       </section>
     `;
   }
@@ -118,7 +415,23 @@ export class AppRoot extends LitElement {
     :host {
       display: block;
       position: relative;
-      color: #fff;
+      font-size: 16px;
+    }
+
+    button,
+    input {
+      height: 40px;
+      font-size: 20px;
+    }
+
+    form {
+      display: inline-block;
+      margin: 10px auto;
+    }
+
+    input {
+      min-width: 250px;
+      margin-left: 10px;
     }
 
     .details {
@@ -131,6 +444,61 @@ export class AppRoot extends LitElement {
       margin: 10px auto;
       border: 1px solid green;
       text-align: center;
+    }
+
+    iframe {
+      display: block;
+      border: 1px solid pink;
+      min-height: 500px;
+      width: 100%;
+    }
+
+    dl dr {
+      display: flex;
+      font-size: 20px;
+      border-bottom: 1px solid;
+      padding: 5px;
+      align-items: center;
+    }
+
+    dl dr dt {
+      width: 200px;
+    }
+
+    #error {
+      background-color: red;
+      color: white;
+    }
+
+    #demo-clicks {
+      border: 3px solid green;
+      padding: 5px;
+      max-height: 200px;
+      overflow-y: auto;
+    }
+
+    .demo {
+      border: 1px solid rebeccapurple;
+      display: inline-block;
+      height: 200px;
+      width: 200px;
+      padding: 5px;
+      overflow: auto;
+      margin: 5px;
+    }
+
+    .demo.selected {
+      background-color: lavender;
+    }
+
+    .demo-go {
+      display: block;
+      width: 100%;
+    }
+
+    .album-image {
+      height: 200px;
+      border: 1px solid;
     }
   `;
 }
---
+++ src/models/album.ts
@@ -0,0 +1,376 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable max-classes-per-file */
+import {
+  File,
+  MetadataResponse,
+  StringField,
+} from '@internetarchive/search-service';
+import { PlaylistTrack, Spectrogram, Track, TrackDetails } from './track';
+
+/**
+ * An Album on the Internet Archive
+ *
+ * Takes a MetadataResponse & PlaylistTrack[]
+ * to create an Album
+ * */
+export class Album {
+  tracks: PlaylistTrack[] = [];
+
+  images: File[] = [];
+
+  linerNotes: File[] = [];
+
+  tracksAreSegmented: boolean = false;
+
+  baseHost: string = 'archive.org';
+
+  relatedFiles: File[] = [];
+
+  rawPlaylistTracks: PlaylistTrack[] = [];
+
+  item: MetadataResponse;
+
+  constructor(item: MetadataResponse, tracks: PlaylistTrack[]) {
+    this.item = item;
+    this.rawPlaylistTracks = tracks;
+    this.tracks = tracks;
+    this.filterFiles(this.item.files as File[]);
+  }
+
+  get title(): string {
+    return (this.item.metadata?.title?.values.join('; ') as string) || '';
+  }
+
+  get creator(): StringField | string {
+    if (this.item.metadata?.creator?.values) {
+      return this.item.metadata?.creator?.values.join('; ');
+    }
+
+    if (this.item.metadata.rawMetadata?.artist) {
+      return this.item.metadata.rawMetadata?.artist;
+    }
+    return '';
+  }
+
+  get youtubeId(): string {
+    return (
+      (this.externalIds as string[]).find((e: string) =>
+        e.match(/youtube/gi)
+      ) || ''
+    );
+  }
+
+  get spotifyId(): string {
+    return (
+      (this.externalIds as string[]).find((e: string) =>
+        e.match(/spotify/gi)
+      ) || ''
+    );
+  }
+
+  get externalIds(): string[] {
+    if (!this.item.metadata.rawMetadata) {
+      return [''];
+    }
+
+    const exId = this.item.metadata['external-identifier'] as StringField;
+    return exId?.values?.length ? exId.values : [exId?.value || ''];
+  }
+
+  get youtubeTracks(): Track[] {
+    const availableTracks = this.tracks.filter((tr: Track) => tr.youtubeId);
+
+    if (this.albumTrackOption('yt') !== undefined) {
+      availableTracks.unshift(this.albumTrackOption('yt') as PlaylistTrack);
+    }
+    return availableTracks;
+  }
+
+  get spotifyTracks(): Track[] {
+    const availableTracks = this.tracks.filter((tr: Track) => tr.spotifyId);
+
+    if (this.albumTrackOption('sp') !== undefined) {
+      availableTracks.unshift(this.albumTrackOption('sp') as PlaylistTrack);
+    }
+    return availableTracks;
+  }
+
+  get albumImage(): string {
+    // #1 - default Item Image
+    const itemImage = this.images.find(
+      (img: File) => img.format === 'Item Image'
+    );
+    const urlBase = `https://${this.baseHost}/download/${this.item.metadata.identifier}/`;
+    if (itemImage) {
+      return `${urlBase}${itemImage.name}`;
+    }
+
+    // #2 - originally uploaded image
+    const originalImages = this.images.find((img: File) => {
+      const invalidFormats = ['Item Image', 'JPEG Thumb'];
+      return img.source === 'original' && !invalidFormats.includes(img.format);
+    });
+    if (originalImages) {
+      return `${urlBase}${originalImages.name}`;
+    }
+
+    // #3 - thumbnail
+    return `${urlBase}__ia_thumb.jpg`;
+  }
+
+  albumTrackOption(playerType: 'yt' | 'sp'): Track | undefined {
+    const base = {
+      title: 'Full Album',
+      orig: '',
+      image: '',
+      duration: '-- : --',
+      track: '0',
+      sources: [],
+    };
+
+    if (playerType === 'yt' && this.youtubeId) {
+      return { ...base, youtubeId: this.youtubeId } as unknown as Track;
+    }
+
+    if (playerType === 'sp' && this.spotifyId) {
+      return { ...base, spotifyId: this.spotifyId } as unknown as Track;
+    }
+
+    return undefined;
+  }
+
+  isAlbumRelatedFile(file: File): boolean {
+    return (
+      !!file.name.match(/.(ffp|md5)$/g)?.length &&
+      !!file.original?.length &&
+      file.source === 'derivative'
+    );
+  }
+
+  isValidAudioFile(trackName: string = ''): boolean {
+    return !!trackName.match(
+      /(mp3|ogg|flac|m4a|wma|aiff|aac|aa|ra|ram|shn|wav|wave|opus)$/gi
+    )?.length;
+  }
+
+  isValidImageFile(imageName: string = ''): boolean {
+    return !!imageName.match(/.(png|jpg|jpeg)$/gi)?.length;
+  }
+
+  isValidImageFileFormat(imageFormat: string = ''): boolean {
+    return !!imageFormat.match(/(png|jpg|jpeg)/gi)?.length;
+  }
+
+  isValidSegmentFile(fileOrigin: string = ''): boolean {
+    return !!fileOrigin.match(/_segments.(json|xml)$/gi)?.length;
+  }
+
+  isSpectrogram(fileName: string = ''): boolean {
+    return !!fileName.match(/spectrogram/gi)?.length;
+  }
+
+  isSampleMP3(fileName: string = ''): boolean {
+    return !!fileName.match(/_sample\.mp3$/gi)?.length;
+  }
+
+  hasScannedLinerNotes(fileName: string = ''): boolean {
+    return !!fileName.match(/_jp2.(zip|tar)/gi)?.length;
+  }
+
+  // tracksEntry(file: File): PlaylistTrack | undefined {
+  //   return this.tracks.find(tr => {
+  //     if (file.original) {
+  //       // segmented track
+  //       return tr.orig === file.original;
+  //     }
+  //     // original audio track
+  //     return tr.orig === file.name;
+  //   });
+  // }
+
+  /**
+   * Fills sorted track list properties.
+   * During `MetadataResponse.files` walk, we identify TrackDetails
+   * to enrich track information received from `PlaylistTrack[]`
+   */
+  filterFiles(files: File[]): void {
+    const images: File[] = [];
+    const topLevelTracks = {} as Record<string, TrackDetails>;
+
+    files.forEach((file: File): void => {
+      /* pre-checks before discarding non-audio/image files */
+      const f = file.rawValue as File;
+      const isAudioFile = this.isValidAudioFile(f.name);
+
+      if (this.isAlbumRelatedFile(f)) {
+        this.relatedFiles.push(f);
+        return;
+      }
+
+      if (this.isValidSegmentFile(f.name)) {
+        // check for segmentation - a sign for LPs, 78s
+        this.tracksAreSegmented = true;
+        this.relatedFiles.push(f);
+
+        // set pointer
+        if (
+          f.original &&
+          this.isValidAudioFile(f.name) &&
+          !topLevelTracks[f.original]
+        ) {
+          topLevelTracks[f.original] = {
+            primary: undefined,
+            spectrogram: undefined,
+            related: [],
+            sampleMp3: undefined,
+            fullMp3: undefined,
+          } as unknown as TrackDetails;
+        }
+      }
+
+      if (f.source === 'original' && !topLevelTracks[f.name] && isAudioFile) {
+        topLevelTracks[f.name] = {
+          primary: undefined,
+          spectrogram: undefined,
+          related: [],
+          sampleMp3: undefined,
+          fullMp3: undefined,
+        } as unknown as TrackDetails;
+      }
+
+      if (
+        f.original &&
+        this.isValidAudioFile(f.original) &&
+        !topLevelTracks[f.original]
+      ) {
+        // if no track bucket, let's start one
+        topLevelTracks[f.original] = {
+          primary: undefined,
+          spectrogram: undefined,
+          related: [],
+          sampleMp3: undefined,
+          fullMp3: undefined,
+        } as unknown as TrackDetails;
+      }
+
+      if (this.hasScannedLinerNotes(f.name)) {
+        this.linerNotes.push(f);
+      }
+
+      if (this.isValidImageFile(f.name)) {
+        // grab spectorgrams, they are always derivatives
+        // so they will always have an `original` field
+        if (f.original && this.isValidAudioFile(f.original)) {
+          topLevelTracks[f.original].spectrogram = f as Spectrogram;
+        } else {
+          // find all album images
+          images.push(f);
+        }
+        return;
+      }
+
+      if (
+        f.original &&
+        this.isValidAudioFile(f.original) &&
+        !this.isValidAudioFile(f.name)
+      ) {
+        // group related, non audio files
+        topLevelTracks[f.original].related.push(f);
+        return;
+      }
+
+      if (
+        !this.isValidAudioFile(f.name) ||
+        (!this.isValidImageFile(f.name) && !this.isValidAudioFile(f.name))
+      ) {
+        // discard non-audio/image files
+        return;
+      }
+
+      /** All audio from here */
+
+      // Identify top level track and set it
+      const segmentedTrack =
+        this.isValidSegmentFile(f.original) && this.isValidAudioFile(f.name);
+      const primaryTrack =
+        f.source === 'original' && this.isValidAudioFile(f.name);
+
+      if (primaryTrack || segmentedTrack) {
+        if (!topLevelTracks[f.name]) {
+          topLevelTracks[f.name] = {
+            primary: undefined,
+            spectrogram: undefined,
+            related: [],
+            sampleMp3: undefined,
+            fullMp3: undefined,
+          } as unknown as TrackDetails;
+        }
+        // pin top track file
+        topLevelTracks[f.name].primary = f;
+        return;
+      }
+
+      if (f.name.match(/_sample\.mp3$/)?.length && f.original) {
+        // get samples
+        topLevelTracks[f.original].sampleMp3 = new File(f);
+        return;
+      }
+
+      if (
+        this.isValidAudioFile(f.name) &&
+        !this.isSampleMP3(f.name) &&
+        this.isValidAudioFile(f.original) &&
+        f.original
+      ) {
+        // if not sample but is mp3 & has original reference
+        // that is the full MP3
+        if (f.name.match(/.mp3$/)?.length) {
+          topLevelTracks[f.original].fullMp3 = f;
+        } else {
+          topLevelTracks[f.original].related.push(f);
+        }
+      }
+    }); // end files walk
+
+    // grab images
+    this.images = images;
+
+    // grab detailed tracks
+    const allTracks: PlaylistTrack[] = [];
+    this.tracks.forEach((tr: PlaylistTrack, i) => {
+      const primaryTrack = topLevelTracks[tr.orig].primary;
+
+      const detailedTrackInfo = new Track(
+        { ...tr, ...primaryTrack, details: topLevelTracks[tr.orig] },
+        'archive.org'
+      ) as PlaylistTrack;
+      detailedTrackInfo.setPlaylistInfo(tr);
+
+      allTracks[i] = detailedTrackInfo;
+    });
+    this.tracks = allTracks;
+  }
+}
+
+/*
+Noes on the two primary ways we archive audio items:
+
+File relationships:
+
+### Primary - Scanned CDs, Audio Uploads
+1 album = N audo files, 1 song = 1 track
+  - derived (full) MP3s
+  - derived (sample) MP3s
+  - youtube id
+  - spotify id
+  - spectrogram
+
+### Segmented - Scanned LPs, Scanned 78s
+Original audio is Long Play, must be segmented to retrieve tracks.
+1 album file = 1 audio file => post processed to retrieve many tracks
+- derived segmented tracks (MP3s)
+  - derived (sample) MP3s
+  - spectrogram
+  - youtube id
+  - spotify id
+*/
