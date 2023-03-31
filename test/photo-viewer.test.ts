b86d21534a42e9d835161b645ad5b7db820c7286
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2023-03-31 00:03:24 -0700

    update unit test

--- test/photo-viewer.test.ts
+++ test/photo-viewer.test.ts
@@ -89,16 +89,23 @@ describe('`<iaux-photo-viewer>`', () => {
     });
   });
   describe('Defaults', () => {
-    it('Displays Audio Icon as default', async () => {
+    it('Displays `<iamusic-noimage>` as default', async () => {
       const el = await fixture<IaPhotoViewer>(
         html`<iaux-photo-viewer></iaux-photo-viewer>`
       );
 
       expect(el.shadowRoot).to.exist;
       expect(el.shadowRoot?.querySelector('div.no-images')).to.exist;
-      expect(el.shadowRoot?.querySelector('ia-icon-audio')).to.exist;
-      expect(el.shadowRoot?.querySelector('ia-icon-audio')?.shadowRoot).to
-        .exist;
+      expect(
+        el.shadowRoot
+          ?.querySelector('div.no-images')
+          ?.querySelector('iamusic-noimage')
+      ).to.exist;
+      expect(
+        el.shadowRoot
+          ?.querySelector('iamusic-noimage')
+          ?.shadowRoot?.querySelector('ia-icon-audio')?.shadowRoot
+      ).to.exist;
     });
   });
 
